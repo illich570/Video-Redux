1e03eb34a9d11f20b3dc794103d8365b23dbd000
Illich Rada <illich570@gmail.com>
2021-08-25 20:00:12 -0400

    [ADD] added routing

---
+++ src/containers/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initalState';
+
+const Home = () => {
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <div className='App'>
+      <Search />
+      {
+        initialState.mylist.length > 0 &&
+      (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {initialState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+          </Carousel>
+        </Categories>
+      )
+      }
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/styles/components/NotFound.scss'
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>Pagina No encontrada!</h1>
+    </div>
+  )
+}
+
+export default NotFound
